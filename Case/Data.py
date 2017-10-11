f897ca2abb64e66c365673e56a1cfb7fae8cf575
rrmiracle <lirr@llvision.com>
2017-10-11 18:17:10 +0800

    update

--- Case/Data.py
+++
@@ -1,9 +0,0 @@
-class Data:
-    url = "http://dev.llvision.com:25008"
-    username = "lirr@llvision.com"
-    password = "123456"
-    username_deleted = "test111@qq.com"
-    password_deleted = "1"
-    register_url = "http://dev.llvision.com:25008/register.html"
-    main_url = "http://dev.llvision.com:25008/index.html"
-
